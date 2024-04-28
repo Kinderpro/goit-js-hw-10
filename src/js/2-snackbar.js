63da3d95cd4429da7da964e280729f0a72f6503f
Kinderpro <131799650+Kinderpro@users.noreply.github.com>
2024-04-28 11:48:06 +0300

    update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -10,77 +10,31 @@ function formSubmit(e) {
   const delay = form.elements.delay.value;
 
   const radioState = this.elements.state.value;
-  setTimeout(() => {
-    if (radioState === 'fulfilled') {
-      return Promise.resolve(delay)
-        .then(delay =>
-          iziToast.success({
-            title: 'OK',
-            message: `✅ Fulfilled promise in ${delay}ms`,
-            position: 'topCenter',
-          })
-        )
-        .catch(delay =>
-          iziToast.error({
-            title: 'Error',
-            message: `❌ Rejected promise in ${delay}ms`,
-            position: 'topCenter',
-          })
-        );
-    } else {
-      return Promise.reject(delay)
-        .then(delay =>
-          iziToast.success({
-            title: 'OK',
-            message: `✅ Fulfilled promise in ${delay}ms`,
-            position: 'topCenter',
-          })
-        )
-        .catch(delay =>
-          iziToast.error({
-            title: 'Error',
-            message: `❌ Rejected promise in ${delay}ms`,
-            position: 'topCenter',
-          })
-        );
-    }
-  }, delay);
-
-  //   creatPromise(radioState, delay)
-  //     .then(({ delay }) => console.log(`✅ Fulfilled promise in ${delay}ms`))
-  //     .catch(({ delay }) => console.error(`❌ Rejected promise in ${delay}ms`));
+  creatPromise(radioState, delay)
+    .then(({ delay }) =>
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topCenter',
+      })
+    )
+    .catch(({ delay }) =>
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topCenter',
+      })
+    );
 }
 
-// function creatPromiseFul(position, delay) {
-//   return setTimeout(() => {
-//     Promise.resolve(position, delay);
-//   }, delay);
-// }
-
-// function creatPromiseRej(position, delay) {
-//   return setTimeout(() => {
-//     Promise.reject(position, delay);
-//   }, delay);
-// }
-// function creatPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-
-//       if (position) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
-
-// function creatPromise(position, delay) {
-//   setTimeout(() => {
-//     if (position) {
-//       return Promise.resolve({ delay });
-//     } else {
-//       return Promise.reject({ delay });
-//     }
-//   }, delay);
-// }
+function creatPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (position === 'fulfilled') {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
